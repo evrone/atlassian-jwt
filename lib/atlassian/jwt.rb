21094dac3dbc447a489031f230733cbff8086de4
Spike Ilacqua <spike@indra.com>
2016-08-15 16:12:16 -0700

    Initial gem setup

---
+++ lib/atlassian/jwt.rb
@@ -0,0 +1,7 @@
+require "atlassian/jwt/version"
+
+module Atlassian
+  module Jwt
+    # Your code goes here...
+  end
+end
---
+++ spec/atlassian/jwt_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Atlassian::Jwt do
+  it 'has a version number' do
+    expect(Atlassian::Jwt::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
