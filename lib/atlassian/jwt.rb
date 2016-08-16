d0410e6495c5ccc643204a088d11c0ebfbf318ac
Spike Ilacqua <spike@indra.com>
2016-08-15 20:22:12 -0700

    Canonical URL checkpoint

--- lib/atlassian/jwt.rb
+++ lib/atlassian/jwt.rb
@@ -4,9 +4,41 @@ require 'jwt'
 module Atlassian
   module Jwt
     class << self
+      CANONICAL_QUERY_SEPARATOR = '&'
+      ESCAPED_CANONICAL_QUERY_SEPARATOR = '%2F'
+
       def decode(token,secret, validate = true, options = {})
         options.merge({:algorithm => 'HS256'})
         ::JWT.decode token, secret, validate, options
+      end
+
+      def create_canonical_request(uri,http_method,base_uri)
+        uri = URI.parse(uri) unless uri.kind_of? URI
+        base_uri = URI.parse(base_uri) unless base_uri.kind_of? URI
+
+        path = canonicalize_uri(uri, base_uri)
+
+        [http_method.upcase,
+         canonicalize_uri(uri, base_uri),
+         canonicalize_query_string(uri.query)
+        ].join(CANONICAL_QUERY_SEPARATOR)
+      end
+
+      def canonicalize_uri(uri, base_uri)
+        path = uri.path.sub(/^#{base_uri.path}/,'')
+        path = '/' if path.nil? || path.empty?
+        path = '/' + path unless path.start_with? '/'
+        path.chomp!('/') if path.length > 1
+        path.gsub(CANONICAL_QUERY_SEPARATOR,ESCAPED_CANONICAL_QUERY_SEPARATOR)
+      end
+
+      def canonicalize_query_string(query)
+        return '' if query.nil? || query.empty?
+        query = CGI::parse(query)
+        query.delete('jwt')
+        p query
+        query = Hash[query.sort]
+        URI.encode_www_form query
       end
     end
   end
--- spec/atlassian/jwt_spec.rb
+++ spec/atlassian/jwt_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'json'
 
+BASE_URL = ''
+
 describe Atlassian::Jwt do
   it 'has a version number' do
     expect(Atlassian::Jwt::VERSION).not_to be nil
@@ -12,7 +14,7 @@ describe Atlassian::Jwt do
   test_data = JSON.parse(json_tests)
   shared_secret = test_data['secret']
     test_data['tests'].each do |test|
-      it "#{test['name']}" do
+      it "#{test['name']} - Decode" do
         uri = URI.parse(test['signedUrl'])
         query = CGI::parse(uri.query)
         token = query['jwt'].first
@@ -20,6 +22,13 @@ describe Atlassian::Jwt do
                               shared_secret,
                               true,
                               { leeway: (3600 * 24 * 365 * 10)})
+      end
+      it "#{test['name']} - Canonical URL" do
+        canonical_uri = Atlassian::Jwt.create_canonical_request(test['signedUrl'],'GET',BASE_URL)
+
+        # Remote the jwt query param from the signed URL to get the original
+
+        expect(canonical_uri).to eq test['canonicalUrl']
       end
     end
 end
